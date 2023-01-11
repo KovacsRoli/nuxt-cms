f74ee21e70a234a2a55b27cd233549e835180823
Kovacs Roland <kovacsroland1999@gmail.com>
2023-01-11 21:19:17 +0200

    Set up dependecies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  typescript: {
+    shim: false, // disable generating the shim for *.vue files
+    strict: true,
+  },
+  modules: ['@nuxtjs/tailwindcss']
+});
